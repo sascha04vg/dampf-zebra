d4d6495ffe216ef3682b798d1324660c319649c9
Sascha Van Geete <svangeete@aptean.com>
2020-10-12 20:08:33 +0100

    image resize

--- index.js
+++ index.js
@@ -52,37 +52,3 @@ window.addEventListener("scroll", function () {
     }
   }
 });
-
-function initMap() {
-  // The location of Ubach
-  var ubach = { lat: 50.924903, lng: 6.101769 };
-  // The map, centered at Ubach
-  var map = new google.maps.Map(document.getElementById("map-ubach"), {
-    zoom: 16,
-    center: ubach,
-  });
-  // The marker, positioned at Eschweiler
-  var marker = new google.maps.Marker({ position: ubach, map: map });
-  // The location of Eschweiler
-  var eschweiler = { lat: 50.822623, lng: 6.24575 };
-  // The map, centered at Eschweiler
-  var map = new google.maps.Map(document.getElementById("map-eschweiler"), {
-    zoom: 16,
-    center: eschweiler,
-  });
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({ position: eschweiler, map: map });
-
-  // The location of Eschweiler
-  var julich = { lat: 50.921144, lng: 6.356142 };
-  // The map, centered at Eschweiler
-  var map = new google.maps.Map(document.getElementById("map-julich"), {
-    zoom: 16,
-    center: julich,
-  });
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({
-    position: julich,
-    map: map,
-  });
-}
